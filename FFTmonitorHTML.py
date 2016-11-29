4c904a26f31ab07b1d971eccca854402dd309dfd
victorpepp <victorpimenta@poli.ufrj.br>
2016-11-29 18:37:32 -0200

    Update FFTmonitorHTML.py
    1- Added peak detection library created by Lucas Hermann Negri (MIT
    License)
    2- Updated plots containing peaks
    3- Minor bug fixes and improvements

--- FFTmonitorHTML.py
+++ FFTmonitorHTML.py
@@ -11,6 +11,7 @@ from scipy.fftpack import fft
 from tkinter import *
 from tkinter import filedialog
 import pandas as pd
+import peak
 
 import plotly
 import plotly.plotly as py
@@ -21,7 +22,7 @@ plotly.tools.set_credentials_file(username='victorpimenta', api_key='znZcYtEF4Mq
 Variables
 """
 
-threshold = 0.8 # Alterar segundo a norma ISO 2372
+threshold = 0.05 # Changes according to standards (ISO 2372)
 
 
 #%%
@@ -33,21 +34,21 @@ functions space
 
 #create a sin function signal with 3 base frequencies and its 3 first harmonics
 def signalgenerator(frequencies, Fs, noise = True, signal = 0):
-    
+
     for freq in frequencies:
         #base frequencies
         wave = (amplitude) * np.sin(2.0*np.pi*freq*t)
-        
+
         #1st harmonics
         harmonic1 = (amplitude/2) * np.sin(2.0*np.pi*2*freq*t)
-        
+
         signal += wave + harmonic1
 
     #creates noise
     # 0 is the mean of the normal distribution you are choosing from
     # 1 is the standard deviation of the normal distribution
     # last one is the number of elements you get in array noise
-    
+
     noise = np.random.normal(0,1,Fs)
 #TODO: create a method that generates more waves and maybe add noise
 
@@ -64,23 +65,33 @@ def csv_data(file_path):
 
 
 
-################## freqZoom #######################
+################ findNearest ######################
+
+# Given a number and an array, finds the index of the nearest value inside the array
+def findNearest(value, array):
+
+    index = np.abs(array - value).argmin()
+    return index
+
+
+############### freqZoom #######################
 
 # Given 2 frequency values, creates a plot of the FFT in the interval
 # delimited by them
-def freqZoom(yf, xf, lowFreq, highFreq, limit = False):
-    
-    N = np.int(np.prod(yf.shape)) # Attention to yf.shape, which will supply (total N)/2
-    ax = plt.figure().add_subplot(111)  
-    ax.plot(xf[int(lowFreq*N/Fs) : int(highFreq*N/Fs) + 1], 2.0/N * np.abs(yf[int(lowFreq*N/Fs):int(highFreq*N/Fs) + 1]))
+def freqZoom(yf, xf, lowFreq, highFreq, threshold = False):
+
+    lowFreqIndex = findNearest(lowFreq, xf)
+    highFreqIndex = findNearest(highFreq, xf)
+    ax = plt.figure().add_subplot(111)
+    ax.plot(xf[lowFreqIndex:highFreqIndex], yf[lowFreqIndex:highFreqIndex])
     ax.grid()
     ax.set_xlabel('Frequency (Hz)')
     ax.set_ylabel('Amplitude')
     #ax.set_xlim([int(lowFreq), int(highFreq)])
     message = "Ok"
-    if limit != False:     
-        ax.hlines(limit, int(lowFreq), int(highFreq) + 1, color = 'r')
-        if np.max(2.0/N * np.abs(yf[int(lowFreq*N/Fs):int(highFreq*N/Fs) + 1])) >= limit:
+    if threshold != False:
+        ax.hlines(threshold, int(lowFreq), int(highFreq), color = 'r')
+        if np.max(yf[lowFreqIndex:highFreqIndex]) >= threshold:
             message = "Danger"
     ax.set_title("%.1f Hz to %.1f Hz - %s"%(lowFreq,highFreq,message))
 
@@ -99,7 +110,7 @@ amplitude = 1.0
 #in Hz
 frequencies = [1000.0, 755.0, 355.0]
 
-#t = np.arange(0.0,1,25e-6)  
+#t = np.arange(0.0,1,25e-6)
 
 ####################################
 
@@ -135,13 +146,22 @@ print("# Samples:", N)
 
 #%%
 """
-FFT
+FFT and Peak Detection
 """
 
 yf = np.fft.fft(signal)
 xf = np.fft.fftfreq(t.shape[-1], T)
 xf = xf[0:np.int(N/2)]
 yf = 2.0/N * np.abs(yf[0:np.int(N/2)])
+
+max_index = xf[yf.argmax()]
+min_dist = findNearest(max_index, xf)
+
+peaks = peak.indexes(yf, thres = 0.25, min_dist = min_dist)
+
+# Threshold normalizes values to a number between 0 and 1
+# Minimum distance between peaks will be adopted as 60% of the biggest amplitude (other peaks probably will be its harmonics)
+
 
 
 #%%
@@ -173,6 +193,8 @@ plt.clf()
 fig2 = plt.figure(2)
 ax2 = fig2.add_subplot(111)
 ax2.plot(xf, yf)
+ax2.plot(xf[peaks], yf[peaks], 'r+', ms = 5, mew = 2, label ='{} peaks'.format(len(peaks)))
+ax2.legend()
 ax2.grid()
 ax2.set_xlabel('Frequency (Hz)')
 ax2.set_ylabel('Amplitude')
@@ -180,11 +202,11 @@ ax2.set_title("Frequency Domain")
 plt.savefig('htmlreport/img/freq.png')
 
 #freqZoom(yf,xf,0,1500, 0.8)
-freqZoom(yf,xf,0,2000,0.05)
+freqZoom(yf,xf,0,2000,threshold)
 
 plt.show()
 
-#%%
+    #%%
 """
 Plotly Dynamic Plot
 """
@@ -217,17 +239,17 @@ html_string = '''
         <h2>Section 1: Time Domain Signal</h2>
         <img src="img/time.png" alt="Time Domain Signal" style="width:432px;height:288px;">
         <p>Include maximum value, average, etc</p>
-        
+
         <!-- *** Section 2 *** --->
         <h2>Section 2: FFT Spectrogram (Fast Fourier Analysis)</h2>
         <<img src="img/time.png" alt="Time Domain Signal" style="width:432px;height:288px;">
         <p>Include maximum value, average, etc</p>
-        
+
         <h3>Reference table: rms value</h3>
         ''' + html_table + '''
     </body>
 </html>'''
-    
+
 f = open('htmlreport/report.html','w')
 f.write(html_string)
 f.close()
