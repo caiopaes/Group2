3373b4f9e561932ff93e8679d7ac9fd5f6ddfc14
victorpepp <victorpimenta@poli.ufrj.br>
2016-11-30 16:18:23 -0200

    Inserted plots into HTML source

--- FFTmonitorHTML.py
+++ FFTmonitorHTML.py
@@ -443,19 +443,10 @@ html_string = '''
         <!-- Left col -->
         <section class="col-lg-12 connectedSortable">
           <!-- Custom tabs (Charts with tabs)-->
-          <div class="nav-tabs-custom">
-            <!-- Tabs within a box -->
-            <ul class="nav nav-tabs pull-right">
-              <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
-              <li><a href="#sales-chart" data-toggle="tab">Donut</a></li>
-              <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li>
-            </ul>
-            <div class="tab-content no-padding">
-              <!-- Morris chart - Sales -->
-              <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 500px;"></div>
-              <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 500px;"></div>
-            </div>
-          </div>
+          
+          <img class="img-responsive" src="img/time.png" alt="FFT" style="width:50%" align="left">
+          <img class="img-responsive" src="img/freq.png" alt="FFT" style="width:50%" align="right">
+          
         </section>
         <!-- /.Left col -->
         <!-- right col (We are only adding the ID to make the widgets sortable)-->
